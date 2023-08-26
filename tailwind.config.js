197fc37831d0dc0342a0524d0868a2acf44d2d02
Marcos Augusto <marcos.argos119@gmail.com>
2023-08-26 20:00:36 -0300

    stylesheet/ home

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: ["./medicSearch/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: { 
+      fontFamily: {
+      inter: ['Inter', 'sans'],
+      },
+      colors: {
+        'bgPrimary': {
+        200: 'rgba(141, 174, 242, 0.71)',
+        900: '#8DAEF2',
+        },
+        'shadowPrimary': {
+        900: 'rgba(136, 163, 220, 1)',
+        },
+      },
   },
   plugins: [],
 }
-
+}
