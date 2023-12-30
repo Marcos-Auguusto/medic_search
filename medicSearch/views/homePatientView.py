97347ca5b33fed7d5c377f5bbe089c1aa6de2735
Marcos Augusto <marcos.argos119@gmail.com>
2023-12-30 01:27:17 -0300

    config/ responsive navbar

--- medicSearch/views/homePatientView.py
+++ medicSearch/views/homePatientView.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home_view(request):
+
+def home_patient(request):
     return render(request, template_name='homePatient.html', status=200)
