8465b163fabebf341e992934e83e4fe63a31146d
Marcos Augusto <marcos.argos119@gmail.com>
2024-02-05 21:55:53 -0300

    Add/ styling to login and registration forms

--- medicSearch/forms/AuthForm.py
+++ medicSearch/forms/AuthForm.py
@@ -3,9 +3,9 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='', required=True,
-                               widget=forms.TextInput())
+                               widget=forms.TextInput(attrs={'class': 'border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full h-11 py-2 px-2'}))
     password = forms.CharField(label='', max_length=32, required=True,
-                               widget=forms.PasswordInput())
+                               widget=forms.PasswordInput(attrs={'class': 'border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full h-11 py-2 px-2'}))
 
 
 class RegisterForm(forms.Form):
