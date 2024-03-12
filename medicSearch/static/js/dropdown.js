9227cc9b7545221622d6443c1cb38382bed37666
Marcos Augusto <marcos.argos119@gmail.com>
2024-03-12 17:06:21 -0300

    Refct/ dropdown functionality and update CSS styles - final

--- medicSearch/static/js/dropdown.js
+++ medicSearch/static/js/dropdown.js
@@ -1,27 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let toggleDropdown = document.getElementById('toggleDropdown');
-    let dropdownMenu = document.getElementById('dropdownMenu');
-    let chevronIcon = document.getElementById('chevronIcon');
+  let toggleDropdown = document.getElementById('toggleDropdown');
+  let dropdownMenu = document.getElementById('dropdownMenu');
+  let chevronIcon = document.getElementById('chevronIcon');
 
-    dropdownMenu.style.display = 'none';
-    chevronIcon.classList.add('transform', 'rotate-0');
+  dropdownMenu.style.display = 'none';
+  chevronIcon.classList.add('rotate-0');
 
-    toggleDropdown.addEventListener('click', () => {
-        dropdownMenu.style.display = dropdownMenu.style.display === 'none' ? 'block' : 'none';
-        if (dropdownMenu.style.display === 'block') {
-            chevronIcon.classList.remove('rotate-0');
-            chevronIcon.classList.add('rotate-180');
-        } else {
-            chevronIcon.classList.remove('rotate-180');
-            chevronIcon.classList.add('rotate-0');
-        }
-    });
+  toggleDropdown.addEventListener('click', () => {
+    dropdownMenu.style.display = dropdownMenu.style.display === 'none' ? 'block' : 'none';
 
-    window.addEventListener('click', event => {
-        if (!toggleDropdown.contains(event.target)) {
-            dropdownMenu.style.display = 'none';
-            chevronIcon.classList.remove('transition-transform', 'duration-500', 'ease-in-out', 'transform', 'hover:rotate-180');
-            chevronIcon.classList.add('rotate-0');
-        }
-    });
+    if (dropdownMenu.style.display === 'block') {
+      chevronIcon.classList.remove('rotate-0');
+      chevronIcon.classList.add('rotate-180');
+    } else {
+      chevronIcon.classList.remove('rotate-180');
+      chevronIcon.classList.add('rotate-0');
+    }
+
+    window.onclick = (e) => {
+      if(!toggleDropdown.contains(e.target)) {
+        dropdownMenu.style.display = 'none';
+        chevronIcon.classList.remove('rotate-180');
+      }
+    }
+  });
+
 });
